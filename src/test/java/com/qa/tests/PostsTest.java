da8f36b0dcad60f659c00dfcfdc8063b0375c847
Marcela Oliveira <magaeu@gmail.com>
2019-10-05 15:04:47 -0300

    add tests by id, all and by user id and not found tests

--- src/test/java/com/qa/tests/PostsTest.java
+++ src/test/java/com/qa/tests/PostsTest.java
@@ -4,9 +4,16 @@ import com.qa.dto.PostDTO;
 import com.qa.setup.BaseTest;
 import com.qa.utils.Helpers;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.response.ResponseBodyData;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.regex.Matcher;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
@@ -21,18 +28,78 @@ public class PostsTest extends BaseTest {
         get("https://jsonplaceholder.typicode.com/posts/")
                 .then()
                 .body(matchesJsonSchemaInClasspath("json/posts.json"));
+
     }
 
     @Test
     public void testGetAllPosts() {
+
+        PostDTO[] retrievedPosts = given()
+                .spec(getReq())
+                .when()
+                .get("posts")
+                .then()
+                .extract()
+                .body()
+                .as(PostDTO[].class);
+
+        assertThat(retrievedPosts).isNotEmpty();
+        assertThat(retrievedPosts.length).isGreaterThan(0);
 
     }
 
     @Test
     public void testGetAllPostsNotFound() {
 
+        Response resp = when()
+                .get("https://jsonplaceholder.typicode.com/postss")
+                .then()
+                .spec(getResp())
+                .extract()
+                .response();
+
+        assertThat(resp.statusCode()).isEqualTo(HttpStatus.SC_NOT_FOUND);
+
     }
 
+    @Test
+    public void testGetPostById() {
+
+        PostDTO post = new PostDTO()
+                .setUserId(3)
+                .setId(21)
+                .setTitle("asperiores ea ipsam voluptatibus modi minima quia sint")
+                .setBody("repellat aliquid praesentium dolorem quo\nsed totam minus non itaque\nnihil labore molestiae sunt dolor eveniet hic recusandae veniam\ntempora et tenetur expedita sunt");
+
+        PostDTO[] retrievedPosts = given()
+                .spec(getReq())
+                .param("id", "21")
+                .when()
+                .get("posts")
+                .then()
+                .extract()
+                .response()
+                .as(PostDTO[].class);
+
+        assertThat(retrievedPosts.length).isGreaterThan(0);
+        assertThat(retrievedPosts[0].getUserId()).isEqualTo(post.getUserId());
+        assertThat(retrievedPosts[0].getId()).isEqualTo(post.getId());
+
+    }
+
+    @Test
+    public void testGetPostByIdNotFound() {
+
+        Response resp = when()
+                .get("https://jsonplaceholder.typicode.com/posts/101")
+                .then()
+                .spec(getResp())
+                .extract()
+                .response();
+
+        assertThat(resp.statusCode()).isEqualTo(HttpStatus.SC_NOT_FOUND);
+
+    }
     @Test
     public void testGetPostsByUserId() {
 
@@ -61,7 +128,20 @@ public class PostsTest extends BaseTest {
     }
 
     @Test
-    public void testGetPostsByUserIdNotFound() {
+    public void testGetPostsByUserIdNotExists() {
+
+        Response resp = when()
+                .get("https://jsonplaceholder.typicode.com/posts?userId=11")
+                .then()
+                .spec(getResp())
+                .extract()
+                .response();
+
+        String jsonBody = resp.getBody().asString();
+        JSONArray jsonArray = new JSONArray(jsonBody);
+
+        assertThat(resp.statusCode()).isEqualTo(HttpStatus.SC_OK);
+        assertThat(jsonArray).isEmpty();
 
     }
 }
